f08841b67b7bdb1d5859d338467d73e0649d6394
Ivan Ilves <ivan.ilves@gmail.com>
2017-02-19 16:55:55 +0100

    Reap client afterwards

--- scripts/darwin/client-setup.sh
+++ scripts/darwin/client-setup.sh
@@ -20,3 +20,11 @@ declare -r CLIENT_IP_ADDR=${IP_BASE}.${CLIENT_LAST_IP_ADDR_OCTET}
 declare -r SERVER_IP_ADDR=${IP_BASE}.${SERVER_LAST_IP_ADDR_OCTET}
 
 sudo ifconfig ${NETWORK_DEVICE} ${CLIENT_IP_ADDR} ${SERVER_IP_ADDR} netmask 255.255.255.255
+
+set +e
+
+NETWORK_SERVICE="$($(dirname ${0})/get-network-service-name.sh)"
+DNS_SERVERS=$(networksetup -getdnsservers "${NETWORK_SERVICE}")
+if [[ "${DNS_SERVERS:0:5}" != "There" ]]; then
+  echo ${DNS_SERVERS} | tee /tmp/xiringuito.dns.${LOCAL_TUNNEL_ID} >/dev/null
+fi
--- scripts/linux/client-teardown.sh
+++ scripts/linux/client-teardown.sh
@@ -1,10 +1,29 @@
 #!/usr/bin/env bash
 #
-# Teardown client after disconnection
+# Teardown client after disconnection [and main program exit]
 #
-if [[ ${#} != 1 ]]; then
-  echo "Usage: ${0} TUNNEL_ID"
+if [[ ${#} -ne 3 ]]; then
+  echo "Usage: ${0} XIRINGUITO_PID SSH_PID TUNNEL_ID"
   exit 1
 fi
 
-sudo ip tuntap del mode tun tun${1}
+if [[ "${USER}" != "root" ]]; then
+  echo "Please run this script by root"
+  exit 77
+fi
+
+declare -r XIRINGUITO_PID=${1}
+declare -r SSH_PID=${2}
+declare -r TUNNEL_ID=${3}
+
+while [[ -d /proc/${XIRINGUITO_PID} ]]; do sleep 1; done
+
+if [[ -f /etc/resolv.conf.orig ]]; then
+  cp /etc/resolv.conf.orig /etc/resolv.conf
+fi
+
+if [[ ${SSH_PID} -ne 0 ]]; then
+  kill ${SSH_PID}; sleep 1
+fi
+
+ip tuntap del mode tun tun${TUNNEL_ID}
