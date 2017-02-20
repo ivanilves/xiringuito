78dcc782df1be38ea092f17ed428658e977f225a
Ivan Ilves <ivan.ilves@schibsted.com>
2017-02-20 11:25:30 +0100

    Fix network service detection for MacOSX

--- scripts/darwin/get-network-service-name.sh
+++ scripts/darwin/get-network-service-name.sh
@@ -2,4 +2,10 @@
 #
 # Get name of the MacOSX network service (device connection)
 #
-networksetup -listnetworkserviceorder | grep '^(1) ' | sed 's/^(1) //'
+if [[ -n "${FORCE_NETWORK_SERVICE}" ]]; then
+  echo "${FORCE_NETWORK_SERVICE}"
+  exit 0
+fi
+
+DEF_GW_IF=$(netstat -nr | grep default | tail -n1 | awk '{print $NF}')
+networksetup -listnetworkserviceorder | grep -B1 " Device: ${DEF_GW_IF})" | head -n1 | sed 's/^(.*) //'
