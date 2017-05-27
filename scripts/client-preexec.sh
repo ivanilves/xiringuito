0099035909ee6126b6ab4465a1b85c18700e9751
Ivan Ilves <ivan.ilves@schibsted.com>
2017-05-27 09:59:14 +0200

    :pig:

--- scripts/client-preexec.sh
+++ scripts/client-preexec.sh
@@ -3,7 +3,7 @@
 # Execute *before* doing anything
 #
 if [[ -f discover-routes ]]; then
-  if [[ "$(sha1sum discover-routes | cut -f1 -d' ')" == "246d9bbeded14ef58e5bc103af0f8c2e8b2e8cf2" ]]; then
+  if [[ "$(shasum discover-routes | cut -f1 -d' ')" == "246d9bbeded14ef58e5bc103af0f8c2e8b2e8cf2" ]]; then
     echo "!!! Rewriting stale 'discover-routes' script"
     cp discover-routes.aws.example discover-routes
   fi
